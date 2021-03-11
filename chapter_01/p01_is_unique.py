b0cba94cd6c0ab3943b5cb1fde2d2e6d09a99532
Dayo Ajayi <dayo.ajayi@gmail.com>
2021-03-11 01:20:18 -0600

    Initial commit

---
+++ chapter_01/p01_is_unique.py
@@ -0,0 +1,10 @@
+# Implement an algorithm to determine if a string has all unique characters. 
+# What if you cannot use additional data structures?
+
+# I: string
+# O: True/False
+# C: no additional data structures, optimize
+# E: empty string
+
+def is_unique(input):
+    pass
