9c0da98b4b2f1537c06be32e7e24ea9c6ec77cfc
Dayo Ajayi <dayo.ajayi@gmail.com>
2021-03-13 22:41:26 -0600

    Solution to problem 1.2

--- chapter_01/p02_check_permutation.py
+++ chapter_01/p02_check_permutation.py
@@ -1,11 +1,20 @@
 # Given two strings, write a function to determine if one is a permutation of the other
 
 # I: Two strings, e.g. "hiiiya", "hiya"
-# O: 
-# C: 
-# E: 
+# O: True/False
+# C: Optimize
+# E: empty string
 
 def check_permutation(inputString1, inputString2):
-    return len(set(inputString1)) == len(inputString2) or len(set(inputString2)) == len(inputString1)
+    if len(inputString1) != len(inputString2):
+        return False
+
+    for char in inputString1:        
+        if char in inputString2:
+            inputString2 = inputString2.replace(char, '')
+    return len(inputString2) == 0
     
-print(check_permutation('hiya', 'hiiiiiya'))
+print(check_permutation('hiiiiiya', 'hiya'))
+print(check_permutation('', ''))
+print(check_permutation('restful', 'fluster'))
+print(check_permutation('car', 'bar'))
